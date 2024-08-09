295c7d655880badb7f2f23d2c0099fec6c7ed002
Matina Pantazidou <Stamatia.Pantazidou@pfizer.com>
2024-08-09 15:29:31 +0300

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ const App: React.FC = () => {
   return (
     <div className="dashboard">
       <div className="profile-card">
-        <h1>User's Profile</h1>
-        {/* Add more profile details here */}
-        <div className="timeframe-buttons">
-          <button onClick={() => setActiveTimeframe('daily')}>Daily</button>
-          <button onClick={() => setActiveTimeframe('weekly')}>Weekly</button>
-          <button onClick={() => setActiveTimeframe('monthly')}>Monthly</button>
-        </div>
+      <div className="avatar">
+        <img src="./images/te.png" alt="User Avatar" className="avatar" />
       </div>
+      <h3>Report for</h3>
+      <h1>Matina Pantazidou</h1>
+      {/* Add more profile details here */}
+      <div className="timeframe-buttons">
+        <button onClick={() => setActiveTimeframe('daily')}>Daily</button>
+        <button onClick={() => setActiveTimeframe('weekly')}>Weekly</button>
+        <button onClick={() => setActiveTimeframe('monthly')}>Monthly</button>
+      </div>
+    </div>
       <div className="activity-cards">
         {timeframes.map((activity: Activity) => (
           <ActivityCard key={activity.title} activity={activity} timeframe={activeTimeframe} />
